639f3032f8242f5b382298113bb705bfff164fe5
Jim Kardach <jim@kardach.clom>
2023-08-07 11:02:17 -0700

    Initial Commit

---
+++ Fin6LEDBot/Fin6LEDBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fin6LEDBotApp.swift
+//  Fin6LEDBot
+//
+//  Created by jim kardach on 8/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Fin6LEDBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
